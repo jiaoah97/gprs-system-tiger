1ae18e14990ac2726766a80c663d4285ad13ffad
mrlgb <MRLGB@SINA.COM>
2012-08-26 01:57:36 +0000

    添加图标筛选的部分代码

---  gprs-system-tiger --username MRLGB@SINA.COM/Tiger-demov2/F_History.Designer.cs
+++  gprs-system-tiger --username MRLGB@SINA.COM/Tiger-demov2/F_History.Designer.cs
@@ -32,11 +32,11 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.entityDataSource1 = new EFWinforms.EntityDataSource(this.components);
+            this._txtMin = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -46,23 +46,20 @@
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.DataSource = this.chartBindingSource;
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 65);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
-            series1.Name = "Time_online ";
-            series1.XValueMember = "UnitID";
-            series1.YValueMembers = "time_online ";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Gprs_state ";
-            series2.XValueMember = "UnitID";
-            series2.YValueMembers = "gprs_state ";
+            series1.Name = "state";
+            series1.XValueMember = "DateTime_RecvDate";
+            series1.YValueMembers = "Temp_HeatingBox";
+            series1.YValuesPerPoint = 2;
             this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(890, 300);
+            this.chart1.Size = new System.Drawing.Size(914, 463);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -73,26 +70,40 @@
             // 
             // chartBindingSource
             // 
-            this.chartBindingSource.DataMember = "tb_union_list";
+            this.chartBindingSource.DataMember = "tb_unit_state";
             this.chartBindingSource.DataSource = this.entityDataSource1;
+            this.chartBindingSource.Filter = "";
             this.chartBindingSource.Position = 0;
+            this.chartBindingSource.Sort = "";
             this.chartBindingSource.ListChanged += new System.ComponentModel.ListChangedEventHandler(this.chartBindingSource_ListChanged);
             // 
             // entityDataSource1
             // 
             this.entityDataSource1.ObjectContextType = typeof(Tiger.db_tigerEntities);
             // 
+            // _txtMin
+            // 
+            this._txtMin.Location = new System.Drawing.Point(202, 13);
+            this._txtMin.Name = "_txtMin";
+            this._txtMin.Size = new System.Drawing.Size(100, 21);
+            this._txtMin.TabIndex = 1;
+            this._txtMin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this._txtMin_KeyPress);
+            this._txtMin.Validated += new System.EventHandler(this._txtMin_Validated);
+            // 
             // F_History
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 463);
+            this.Controls.Add(this._txtMin);
             this.Controls.Add(this.chart1);
             this.Name = "F_History";
             this.Text = "F_history";
+            this.Load += new System.EventHandler(this.F_History_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,5 +112,6 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.BindingSource chartBindingSource;
         private EFWinforms.EntityDataSource entityDataSource1;
+        private System.Windows.Forms.TextBox _txtMin;
     }
 }
---  gprs-system-tiger --username MRLGB@SINA.COM/Tiger-demov2/F_History.cs
+++  gprs-system-tiger --username MRLGB@SINA.COM/Tiger-demov2/F_History.cs
@@ -6,11 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Data.Objects;
 
 namespace Tiger
 {
     public partial class F_History : Form
     {
+        private db_tigerEntities context;
+
         public F_History()
         {
             InitializeComponent();
@@ -18,7 +22,97 @@ namespace Tiger
 
         private void chartBindingSource_ListChanged(object sender, ListChangedEventArgs e)
         {
+            ApplyFilter();
             chart1.DataBind();
+        }
+
+        private void F_History_Load(object sender, EventArgs e)
+        {
+            // show an X label every 3 Minute
+
+            chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Seconds;
+
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "{0:g}";
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+
+            chartArea1.Area3DStyle.Inclination = 15;
+            chartArea1.Area3DStyle.IsClustered = true;
+            chartArea1.Area3DStyle.IsRightAngleAxes = false;
+            chartArea1.Area3DStyle.Perspective = 10;
+            chartArea1.Area3DStyle.Rotation = 10;
+            chartArea1.Area3DStyle.WallWidth = 0;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisX.LabelStyle.Format = "MMM dd";
+            chartArea1.AxisX.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.BackColor = System.Drawing.Color.OldLace;
+            chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.Name = "Default";
+            chartArea1.ShadowColor = System.Drawing.Color.Transparent;
+            this.chart1.ChartAreas.Add(chartArea1);
+
+            context = new db_tigerEntities();
+
+            try
+            {
+                // Create a query for orders and related items.
+                var orderQuery = context.tb_unit_state
+                    .Where("it.UnitID = '13695655652'");
+
+                // Set the data source of the binding source to the ObjectResult 
+                // returned when the query is executed.
+                chartBindingSource.DataSource =
+                    orderQuery.Execute(MergeOption.AppendOnly);
+            }
+            catch (EntitySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // apply the filter
+        private void ApplyFilter()
+        {
+            //// never show discontinued products
+            //var filter = " ";
+
+            //// apply minimum price condition
+            //var minPrice = _txtMin.Text.Trim();
+            //if (!string.IsNullOrEmpty(minPrice))
+            //{
+            //    double d;
+            //    if (!double.TryParse(minPrice, out d))
+            //    {
+            //        MessageBox.Show("Invalid Minimum Unit Price, please try again.");
+            //    }
+            //    else
+            //    {
+            //        filter += string.Format("(Temp_HeatingBox >= {0})", minPrice);
+            //    }
+            //}
+
+            //// set the filter
+            //chartBindingSource.Filter = filter;
+        }
+
+        private void _txtMin_Validated(object sender, EventArgs e)
+        {
+            ApplyFilter();
+        }
+
+        private void _txtMin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ApplyFilter();
+                e.Handled = true;
+            }
         }
     }
 }
